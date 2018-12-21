4c616cb335c3bb390e1faa8c34b45c2819edf45e
Kyle McCullough <kylemcc@gmail.com>
2018-12-21 10:15:53 -0600

    slack: support environment variables in payload

--- slack-webhook/entrypoint.sh
+++ slack-webhook/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [ ! -z "$SLACK_PAYLOAD" ]; then
+	# https://api.slack.com/docs/messages/builder
 	payload=$SLACK_PAYLOAD
 elif [ ! -z "$SLACK_MESSAGE" ]; then
 	payload="{\"username\": \"github-actions\", \"text\": \"$SLACK_MESSAGE\"}"
@@ -9,6 +10,7 @@ fi
 if [ -z "$payload" ]; then
 	echo "SLACK_PAYLOAD or SLACK_MESSAGE must be defined"
 	exit 1
-else
-	curl -XPOST -d"payload=$payload" $SLACK_WEBHOOK_URL
 fi
+
+payload=$(echo $payload | envsubst)
+curl -XPOST -d"payload=$payload" $SLACK_WEBHOOK_URL
